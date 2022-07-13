bcc5f92c877b0b76ddfa53249da0dab33d2301e2
tsuyamakazuki <kazuki_tsuyama@bita.jp>
2022-07-13 16:16:13 +0900

    クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,33 @@ import styles from '../styles/Home.module.css'
 import {Footer} from "../components/Footer/Footer";
 import {Main} from "../components/Main/Main";
 import {Header} from "../components/Header/Header";
+import {useCallback} from "react";
+
+
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault()
+    console.log(foo)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a
+        href={"/about"}
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Main page={"index"}/>
-      <Footer />
+      <Footer/>
     </div>
   )
 }
